47c186661a7f0e828f3a8ff38ddfb10b977d1fb1
Domagoj <domagoj.kriskovic@gmail.com>
2017-04-03 22:39:14 +0200

    device list

--- index.js
+++ index.js
@@ -12,15 +12,63 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const io = require('socket.io')(http);
+const md5Pass = '98c04d88b2886a3ec57a0fb92619d444'
+const md5 = require('blueimp-md5')
+const fs = require('fs')
 
-const state = {
+let state = {
   // device_id: payload
+}
+let deviceList = {
+  // device_id: {
+  //   deviceId: 
+  //   access_token 
+  // }
+}
+
+try {
+  deviceList = JSON.parse(fs.readFileSync("./saved_device_list", 'utf8'))
+} catch (e) {
+  console.log('error parsing saved file')
+  deviceList = {}
 }
 
 const data$ = new Rx.Subject()
 app.post('/message', function (req, res) {
+  if (!deviceList[req.body.bit_id]) {
+    res.sendStatus(403)
+    return
+  }
   state[req.body.bit_id] = req.body
   data$.next(state)
+  res.sendStatus(200)
+})
+
+app.post('/devicelist', function (req, res) {
+  if (md5(req.body.password) !== md5Pass) {
+    res.sendStatus(403)
+    return
+  }
+
+  try {
+    let deviceListStr = JSON.stringify(req.body.device_list)
+    fs.writeFileSync("./saved_device_list", deviceListStr)
+  } catch (e) {
+    res.sendStatus(500)
+    return
+  }
+
+  deviceList = {}
+  state = {}
+
+  req.body.device_list.map(device => {
+    deviceList[device.device_id] = {
+      deviceId: device.device_id,
+      access_token: device.access_token
+    }
+  })
+
+  res.sendStatus(200)
 })
 
 app.get('/status', function (req, res) {
@@ -34,14 +82,6 @@ io.on('connection', function(socket){
     .sample(Rx.Observable.interval(10000))
     .subscribe(function(state) {
       socket.emit('state', state);
-      /*console.log(state)
-      wss.broadcast = function broadcast(state) {
-        wss.clients.forEach(function each(client) {
-          if (client.readyState === WebSocket.OPEN) {
-            client.send(state);
-          }
-        });
-      };*/
     })
 })
 
